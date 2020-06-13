a12b2a4e126978847554a0134f5cda8f5160d705
Mark Delk <jethrodaniel@gmail.com>
2020-06-13 16:34:48 -0500

    init commit

---
+++ fast_inv_sqrt.c
@@ -0,0 +1,24 @@
+// From https://en.wikipedia.org/wiki/Fast_inverse_square_root
+//
+// > ...fast inverse square root implementation from Quake III Arena, stripped
+// > of C preprocessor directives, but including the exact original comment
+// > text
+//
+// Wikipedia sourced if from https://github.com/id-Software/Quake-III-Arena/blob/dbe4ddb10315479fc00086f08e25d968b4b43c49/code/game/q_math.c#L549-L572
+//
+float Q_rsqrt( float number )
+{
+	long i;
+	float x2, y;
+	const float threehalfs = 1.5F;
+
+	x2 = number * 0.5F;
+	y  = number;
+	i  = * ( long * ) &y;                       // evil floating point bit level hacking
+	i  = 0x5f3759df - ( i >> 1 );               // what the fuck?
+	y  = * ( float * ) &i;
+	y  = y * ( threehalfs - ( x2 * y * y ) );   // 1st iteration
+//	y  = y * ( threehalfs - ( x2 * y * y ) );   // 2nd iteration, this can be removed
+
+	return y;
+}
