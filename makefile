b52b145bf246e49c3de433dc2f515f40882a8e5f
Mark Delk <jethrodaniel@gmail.com>
2020-06-14 02:10:52 -0500

    test against 32 and 64 bit

--- makefile
+++ makefile
@@ -1,11 +1,17 @@
-default: test bench
+# sudo apt-get install gcc-multilib
+CFLAGS=-m32
+
+default: bench test
+
+64:
+	$(MAKE) CFLAGS=-m64
 
 test: ctap
-	gcc -o ./test src/fast_inv_sqrt.c spec/fast_inv_sqrt.c && ./test; rm -f ./test
+	gcc $(CFLAGS) -o ./test src/fast_inv_sqrt.c spec/fast_inv_sqrt.c && ./test; rm -f ./test
 
 ctap:
 	git submodule update
 
 bench:
-	gcc src/fast_inv_sqrt.c benchmark/fast_inv_sqrt.c -o $@
+	gcc $(CFLAGS) src/fast_inv_sqrt.c benchmark/fast_inv_sqrt.c -o $@
 	sudo ./$@; rm $@
--- src/fast_inv_sqrt.c
+++ src/fast_inv_sqrt.c
@@ -25,6 +25,26 @@ float Q_rsqrt( float number )
 	return y;
 }
 
+// Same as Q_rsqrt, but use int32_t instead of long.
+//
+// Should have the same time as the original when compiled for 32-bit.
+float quake_3_inv_sqrt_int32( float number )
+{
+	int32_t i;
+	float x2, y;
+	const float threehalfs = 1.5F;
+
+	x2 = number * 0.5F;
+	y  = number;
+	i  = * ( int32_t * ) &y;                       // evil floating point bit level hacking
+	i  = 0x5f3759df - ( i >> 1 );               // what the fuck?
+	y  = * ( float * ) &i;
+	y  = y * ( threehalfs - ( x2 * y * y ) );   // 1st iteration
+//	y  = y * ( threehalfs - ( x2 * y * y ) );   // 2nd iteration, this can be removed
+
+	return y;
+}
+
 // Compute the inverse square root of a non-negative number.
 //
 //     fast_inv_sqrt(25) // 0.2 == 1/√25
